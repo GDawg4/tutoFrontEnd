d1f09722de1cda997810092b0bd02da758c13f53
Rodrigo Garoz <garoz.r4@gmail.com>
2020-05-27 17:57:47 -0600

    Name changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -38,20 +38,20 @@ export const isAuthenticated = state => getAuthToken(state) != null;
 export const getUser = (state, id) => userSelectors.getUser(state.reducer.users, id);
 export const getUsers = state => userSelectors.getUsers(state.reducer.users);
 export const getIsAdding = state => userSelectors.getIsAdding(state.reducer.users);
-export const getAddingError = state => userSelectors.getAddingError(state.reducer.users);
-export const isSuccessful = state => userSelectors.isSuccessful(state.reducer.users);
+export const getAddingErrorUser = state => userSelectors.getAddingError(state.reducer.users);
+export const isSuccessfulUser = state => userSelectors.isSuccessful(state.reducer.users);
 
 export const getAuthor = (state, id) => authorSelectors.getAuthor(state.reducer.authors, id);
 export const getAuthors = state => authorSelectors.getAuthors(state.reducer.authors);
-export const getIsAdding = state => authorSelectors.getIsAdding(state.reducer.authors);
-export const getAddingError = state => authorSelectors.getAddingError(state.reducer.authors);
-export const isSuccessful = state => authorSelectors.isSuccessful(state.reducer.authors);
+export const getIsAddingAuthor = state => authorSelectors.getIsAdding(state.reducer.authors);
+export const getAddingErrorAuthor = state => authorSelectors.getAddingError(state.reducer.authors);
+export const isSuccessfulAuthor = state => authorSelectors.isSuccessful(state.reducer.authors);
 
 export const getTransaction = (state, id) => transactionsSelectors.getTransaction(state.reducer.transactions, id);
 export const getTransactions = state => transactionsSelectors.getTransactions(state.reducer.transactions);
-export const getIsAdding = state => transactionsSelectors.getIsAdding(state.reducer.transactions);
-export const getAddingError = state => transactionsSelectors.getAddingError(state.reducer.transactions);
-export const isSuccessful = state => transactionsSelectors.isSuccessful(state.reducer.transactions);
+export const getIsAddingTransaction = state => transactionsSelectors.getIsAdding(state.reducer.transactions);
+export const getAddingErrorTransaction = state => transactionsSelectors.getAddingError(state.reducer.transactions);
+export const isSuccessfulTransaction = state => transactionsSelectors.isSuccessful(state.reducer.transactions);
 
 export const getCart = state => cartSelectors.getCart(state.reducer.cart);
 export const getIsBookInCart = (state, book) => cartSelectors.isBookInCart(state.reducer.cart, book);

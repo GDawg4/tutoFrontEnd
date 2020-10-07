259977df2d63fd9debde6525e0a4d4707c8c8160
LoboAnimae <andyquan12@gmail.com>
2020-10-06 19:40:45 -0600

    Changed styles for some files

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -9,6 +9,11 @@ import {
   Text,
   Alert,
   StyleSheet,
+  StatusBar,
+  Platform,
+  SafeAreaView,
+  Dimensions,
+  ImageBackground,
 } from 'react-native'
 import { Link, Redirect } from 'react-router-native'
 
@@ -63,214 +68,132 @@ const LoginForm = (props) => {
       'No se puede iniciar sesión con los datos ingresados.'
     )
   }
-
   return (
-    <KeyboardAvoidingView
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        height: '100%',
-        width: '100%',
-        backgroundColor: '#078b45',
-      }}
-    >
-      {/* Headers */}
-      <View
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '20%',
-          width: '100%',
-        }}
-      >
-        {/* Notification Space */}
-        <View
-          style={{
-            width: '100%',
-            height: '15%',
-            position: 'relative',
-            backgroundColor: '#FFF',
-          }}
-        />
-
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            height: '75%',
-            backgroundColor: '#3B3A3C',
-            alignItems: 'flex-start',
-            justifyContent: 'center',
-            
-          }}
-        >
+    <SafeAreaView style={styles.container}>
+      <View style={styles.mainProgram}>
+        {/* Headers */}
+        <View style={styles.headers}>
+          <Image style={{ flex: 1, height: '100%' }} source={logoUVG} />
           <View
             style={{
-              height: '100%',
-              width: '30%',
-              display: 'flex',
+              flex: 3,
+              alignItems: 'center',
               justifyContent: 'center',
-              minHeight: '100%'
             }}
           >
-            <Image
-              style={{
-                height: '60%',
-                width: 'auto',
-              }}
-              source={logoUVG}
-            />
-          </View>
-        </View>
-      </View>
-      {/* Container of information */}
-      <View
-        style={{
-          height: '80%',
-          width: '100%',
-          // backgroundColor: '#',
-          opacity: 1,
-        }}
-      >
-        <View style={styles.imageContainer}>
-          <Image source={cafeteria} style={styles.image} />
-        </View>
-        <View
-          style={
-            isAuthenticating
-              ? [styles.container, styles.authenticating]
-              : styles.container
-          }
-          behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
-        >
-          <View style={styles.textFieldArea}>
-            <Field
-              name={'email'}
-              component={FormTextInput}
-              keyboardType="email-address"
-              placeholder={'Email'}
-              autoCapitalize="none"
-              returnKeyType="next"
-              validate={required}
-            />
-            <Field
-              name={'password'}
-              component={FormTextInput}
-              autoCapitalize="none"
-              secureTextEntry={true}
-              placeholder={'Password'}
-              returnKeyType="done"
-              validate={required}
-            />
-            <Button
-              onPress={handleSubmit}
-              color="#078b45"
-              style={styles.buttonRegister}
-              label={'Iniciar Sesión'}
-              disabled={submitting}
-            />
-            <View style={styles.bottomText}>
+            <View>
               <Text
                 style={{
-                  color: '#FFF',
-                  textAlign: 'center',
+                  fontSize: 35,
+                  color: '#fff',
                 }}
               >
-                ¿No tienes una cuenta?{' '}
+                Tutorías UVG
               </Text>
-              <Link
-                to="/registry"
-                underlayColor="#f0f4f7"
-                style={styles.navItem}
-              >
-                <Text
-                  style={{
-                    color: '#FFF',
-                    textAlign: 'center',
-                  }}
-                >
-                  Haz clic aquí
-                </Text>
-              </Link>
             </View>
-            {isAuthenticating && (
-              <View style={styles.spinner}>
-                <ActivityIndicator size="large" color="#428AF8" />
-              </View>
-            )}
           </View>
         </View>
+        <View style={styles.greenBar} />
+        <View style={styles.content}>
+          <ImageBackground
+            source={cafeteria}
+            blurRadius={3}
+            style={{ flex: 1, width: '100%', justifyContent: 'center' }}
+          >
+            <KeyboardAvoidingView
+              style={(styles.contentContainer, { flexDirection: 'row' })}
+            >
+              <View style={{ flex: 1 }} />
+              <View style={styles.centerBox}>
+                <Field
+                  name={'email'}
+                  component={FormTextInput}
+                  keyboardType="email-address"
+                  placeholder={'Correo'}
+                  autoCapitalize="none"
+                  returnKeyType="next"
+                  validate={required}
+                />
+                <Field
+                  name={'password'}
+                  component={FormTextInput}
+                  autoCapitalize="none"
+                  secureTextEntry={true}
+                  placeholder={'Contraseña'}
+                  returnKeyType="done"
+                  validate={required}
+                />
+                <Button
+                  onPress={handleSubmit}
+                  color="#078b45"
+                  style={styles.buttonRegister}
+                  label={'Iniciar Sesión'}
+                  disabled={submitting}
+                />
+                <Text style={{ textAlign: 'center' }}>
+                  ¿No tienes una cuenta?
+                </Text>
+                <Link to="/registry">
+                  <Text
+                    style={{
+                      textAlign: 'center',
+                      color: '#078b45',
+                      textDecorationLine: 'underline',
+                      textDecorationColor: '#078b45',
+                    }}
+                  >
+                    Registrarse
+                  </Text>
+                </Link>
+              </View>
+              <View style={{ flex: 1 }} />
+            </KeyboardAvoidingView>
+          </ImageBackground>
+        </View>
+        <View style={styles.bottomBar}></View>
       </View>
-    </KeyboardAvoidingView>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#F5F5F5',
-    borderWidth: 1,
-    borderRadius: 10,
-    borderColor: '#000000',
-    position: 'relative',
-    width: '80%',
-    height: 'auto',
-    flexDirection: 'column',
-    alignSelf: 'center',
-    borderColor: 'white',
-    paddingTop: '10%',
-    marginTop: '10%',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    flex: 1,
   },
-  textFieldArea: {
-    width: '80%',
-    height: '80%',
-    alignSelf: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
+  mainProgram: {
+    flex: 1,
   },
-  bodyArea: {
-    height: '81%',
+  headers: {
+    flex: 6,
+    backgroundColor: '#3B3A3B',
+    overflow: 'visible',
+    alignContent: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
   },
-  image: {
-    height: '100%',
-    width: '100%',
-    position: 'relative',
+  greenBar: {
+    flex: 1,
+    backgroundColor: '#078B45',
   },
-  imageContainer: {
-    width: '100%',
-    height: '100%',
+  content: {
+    flex: 27,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  bottomBar: {
     flex: 2,
-    position: 'absolute',
+    backgroundColor: '#3B3A3B',
   },
-  global: {
-    height: '100%',
-    width: '100%',
-    alignItems: 'center',
+  contentContainer: {
+    alignContent: 'center',
+    justifyContent: 'center',
   },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-    position: 'relative',
-  },
-  buttonRegister: {
-    width: '40%',
-    height: '2%',
-    display: 'flex',
-    alignItems: 'center',
-    position: 'relative',
-    alignSelf: 'center',
-    padding: 0,
-    backgroundColor: '#078b45'
-  },
-
-  bottomText: {
-    backgroundColor: '#454545',
-    color: '#FFF',
-    width: '80%',
-    alignSelf: 'center',
-    height: 'auto',
-    borderRadius: 5,
-    padding: 10,
+  centerBox: {
+    flex: 5,
+    backgroundColor: '#fff',
+    padding: '5%',
+    borderRadius: 10,
   },
 })
 
--- src/components/RegistryForm/index.js
+++ src/components/RegistryForm/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { reduxForm, Field } from 'redux-form'
+import logoUVG from '../../assets/logo.png'
 import {
   KeyboardAvoidingView,
   View,
   Text,
   ActivityIndicator,
   Alert,
+  SafeAreaView,
+  StatusBar,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Image,
 } from 'react-native'
 import { Link } from 'react-router-native'
 import { AntDesign } from '@expo/vector-icons'
@@ -14,9 +21,10 @@ import { AntDesign } from '@expo/vector-icons'
 import Button from '../Button'
 import FormTextInput from '../FormTextInput'
 
-import styles from './styles'
+// import styles from './styles'
 import * as userActions from '../../actions/users'
 import * as selectors from '../../reducers'
+import { Platform } from 'react-native'
 
 // funcion de onsubmit
 // envia la informacion ingresada al API para crear un nuevo usuario
@@ -39,40 +47,41 @@ const validate = (values) => {
   const errors = {}
 
   if (!values.name) {
-    errors.name = 'Required'
+    errors.name = 'Requerido'
   }
 
   if (!values.lastname) {
-    errors.lastname = 'Required'
+    errors.lastname = 'Requerido'
   }
 
   if (!values.username) {
-    errors.username = 'Required'
+    errors.username = 'Requerido'
   }
 
   if (!values.age) {
-    errors.age = 'Required'
+    errors.age = 'Requerido'
   }
 
   if (!values.email) {
-    errors.email = 'Required'
+    errors.email = 'Requerido'
   } else if (
     values.email &&
     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
   ) {
-    errors.email = 'Invalid email address'
+    errors.email = 'Correo Electrónico No Válido'
   }
 
   if (!values.password) {
-    errors.password = 'Required'
+    errors.password = 'Requerida'
   } else if (values.password.length < 8) {
-    errors.password = 'Password must be 8 characters long'
+    errors.password =
+      'La contraseña debe de tener un largo mayor a 8 caractéres'
   }
 
   if (!values.confirmpassword) {
-    errors.confirmpassword = 'Required'
+    errors.confirmpassword = 'Requerida'
   } else if (values.confirmpassword !== values.password) {
-    errors.confirmpassword = 'Password mismatched'
+    errors.confirmpassword = 'Las contraseñas no coinciden'
   }
 
   return errors
@@ -99,7 +108,7 @@ const RegistryForm = (props) => {
         />
         <Text>Cuenta creada con exito</Text>
         <Link to="/">
-          <Text style={{ color: '#428AF8' }}>
+          <Text style={{ color: '#078b45' }}>
             Haz clic aquí para iniciar sesión
           </Text>
         </Link>
@@ -119,89 +128,168 @@ const RegistryForm = (props) => {
   }
 
   return (
-    <KeyboardAvoidingView
-      style={
-        isAdding ? [styles.container, styles.authenticating] : styles.container
-      }
-      behavior="padding"
-    >
-      <View style={styles.form}>
-        <Text style={styles.header}>Ingrese sus Datos</Text>
-        <Field
-          name={'name'}
-          component={FormTextInput}
-          placeholder={'Name'}
-          returnKeyType="next"
-          autoCapitalize="words"
-        />
-        <Field
-          name={'lastname'}
-          component={FormTextInput}
-          placeholder={'Last name'}
-          returnKeyType="next"
-          autoCapitalize="words"
-        />
-        <Field
-          name={'email'}
-          component={FormTextInput}
-          keyboardType="email-address"
-          placeholder={'Email'}
-          autoCapitalize="none"
-          returnKeyType="next"
-          autoCompleteType="email"
-        />
-        <Field
-          name={'username'}
-          component={FormTextInput}
-          placeholder={'Username'}
-          autoCapitalize="none"
-          returnKeyType="next"
-        />
-        <Field
-          name={'age'}
-          component={FormTextInput}
-          placeholder={'Age'}
-          keyboardType="numeric"
-          maxLength={2}
-          returnKeyType="next"
-        />
-        <Field
-          name={'password'}
-          component={FormTextInput}
-          autoCapitalize="none"
-          secureTextEntry={true}
-          placeholder={'Password'}
-          returnKeyType="next"
-        />
-        <Field
-          name={'confirmpassword'}
-          component={FormTextInput}
-          autoCapitalize="none"
-          secureTextEntry={true}
-          placeholder={'Confirm Password'}
-          returnKeyType="done"
-        />
-        <Button
-          onPress={handleSubmit}
-          label={'Sign Up'}
-          disabled={submitting}
-        />
-        <View style={styles.bottomText}>
-          <Text style={styles.styledText}>¿Ya tienes una cuenta? </Text>
-          <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
-            <Text style={styles.linkText}>Inicia sesión</Text>
-          </Link>
-        </View>
-        {isAdding && (
-          <View style={styles.spinner}>
-            <ActivityIndicator size="large" color="#428AF8" />
+    <SafeAreaView style={styles.container}>
+      {/* <View style={{ flex: 1 }}> */}
+      <View style={styles.headers}>
+        {/* <Image style={{ flex: 1, height: '100%' }} source={logoUVG} /> */}
+        <View
+          style={{
+            flex: 3,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <View>
+            <Text
+              style={{
+                fontSize: 35,
+                color: '#fff',
+              }}
+            >
+              Registro
+            </Text>
           </View>
-        )}
+        </View>
       </View>
-    </KeyboardAvoidingView>
+      <View style={styles.greenBar} />
+      <View style={{ flex: 29 }}>
+        <ScrollView style={{ flex: 1 }}>
+          <View style={styles.appPaddingCol} />
+          <View style={styles.mainContainer}>
+            <View style={styles.paddingSide} />
+            <View style={styles.scrollViewStyles}>
+              {/* <Text style={styles.header}>Ingrese sus Datos</Text> */}
+              <Field
+                name={'name'}
+                component={FormTextInput}
+                placeholder={'Nombre(s)'}
+                returnKeyType="next"
+                autoCapitalize="words"
+              />
+              <Field
+                name={'lastname'}
+                component={FormTextInput}
+                placeholder={'Apellido(s)'}
+                returnKeyType="next"
+                autoCapitalize="words"
+              />
+              <Field
+                name={'email'}
+                component={FormTextInput}
+                keyboardType="email-address"
+                placeholder={'Correo'}
+                autoCapitalize="none"
+                returnKeyType="next"
+                autoCompleteType="email"
+              />
+              <Field
+                name={'username'}
+                component={FormTextInput}
+                placeholder={'Nombre de Usuario'}
+                autoCapitalize="none"
+                returnKeyType="next"
+              />
+              <Field
+                name={'age'}
+                component={FormTextInput}
+                placeholder={'Edad'}
+                keyboardType="numeric"
+                maxLength={2}
+                returnKeyType="next"
+              />
+              <Field
+                name={'password'}
+                component={FormTextInput}
+                autoCapitalize="none"
+                secureTextEntry={true}
+                placeholder={'Contraseña'}
+                returnKeyType="next"
+              />
+              <Field
+                name={'confirmpassword'}
+                component={FormTextInput}
+                autoCapitalize="none"
+                secureTextEntry={true}
+                placeholder={'Confirmar contraseña'}
+                returnKeyType="done"
+              />
+              <Button
+                onPress={handleSubmit}
+                label={'Registrarse'}
+                color="#078b45"
+                disabled={submitting}
+              />
+              <View style={styles.bottomText}>
+                <Text style={styles.styledText}>¿Ya tienes una cuenta? </Text>
+                <Link to="/" underlayColor="#078b45" style={styles.navItem}>
+                  <Text style={styles.linkText}>Inicia sesión</Text>
+                </Link>
+              </View>
+              {isAdding && (
+                <View style={styles.spinner}>
+                  <ActivityIndicator size="large" color="#428AF8" />
+                </View>
+              )}
+            </View>
+            <View style={styles.paddingSide} />
+          </View>
+          <View style={styles.appPaddingCol} />
+        </ScrollView>
+      </View>
+
+      {/* </View> */}
+    </SafeAreaView>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    flex: 1,
+  },
+  scrollViewStyles: {
+    flex: 10,
+    paddingTop: '10%',
+    justifyContent: 'space-evenly',
+  },
+  paddingSide: {
+    flex: 1,
+  },
+  mainContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  appPaddingCol: {
+    flex: 1,
+  },
+  styledText: {
+    textAlign: 'center',
+    fontSize: 14,
+  },
+  linkText: {
+    textAlign: 'center',
+    color: '#078b45',
+    textDecorationLine: 'underline',
+    textDecorationColor: '#078b45',
+  },
+  header: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  headers: {
+    flex: 3,
+    backgroundColor: '#3B3A3B',
+    overflow: 'visible',
+    alignContent: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  greenBar: {
+    flex: 1,
+    backgroundColor: '#078B45',
+  },
+})
 export default reduxForm({
   form: 'signUp',
   validate,

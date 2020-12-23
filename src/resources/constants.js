b4cddf4750229a49211720aef1c0a15e46ccce29
PingMaster99 <rui18259@uvg.edu.gt>
2020-12-22 18:51:37 -0600

    Push IP

--- src/resources/constants.js
+++ src/resources/constants.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL_WEB = 'http://192.168.0.4:8000/api/v1';
-export const API_BASE_URL_ANDROID = 'http://192.168.0.197:8000/api/v1';
+export const API_BASE_URL_ANDROID = 'http://192.168.0.198:8000/api/v1';
 export const MEDIA_BASE_URL = 'localhost:8000'
